af358af13257c0d833a28768ae36ab6ef3e0cc70
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2023-09-10 19:58:03 -0300

    feat: header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
-import Logo from "./componentes/Logo";
-import OpcoesHeader from "./componentes/OpcoesHeader";
-import IconesHeader from "./componentes/IconesHeader";
+import Header from "./componentes/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Logo />
-        <OpcoesHeader />
-        <IconesHeader />
-      </header>
+      <Header />
     </div>
   );
 }
