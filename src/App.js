2fece2cbbb008a65749bda0b9ff2b1a67aa80a2c
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2023-09-10 19:54:59 -0300

    chore: refactor

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Logo></Logo>
-        <OpcoesHeader></OpcoesHeader>
-        <IconesHeader></IconesHeader>
+        <Logo />
+        <OpcoesHeader />
+        <IconesHeader />
       </header>
     </div>
   );
