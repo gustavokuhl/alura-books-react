a86dd8e4835e66560d1057ce21f6f5958cf77509
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2023-09-12 06:28:19 -0300

    feat: pesquisa de livros

---
+++ src/componentes/Pesquisa/index.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import Input from "../Input";
+import styled from "styled-components";
+import { livros } from "./dadosPesquisa";
+
+const Container = styled.section`
+  background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
+  color: #fff;
+  text-align: center;
+  padding: 85px 0;
+  height: 270px;
+  width: 100%;
+`;
+const Titulo = styled.h2`
+  color: #fff;
+  font-size: 36px;
+  text-align: center;
+  width: 100%;
+`;
+const Subtitulo = styled.h3`
+  font-size: 16px;
+  font-weight: 500;
+  margin-bottom: 40px;
+`;
+const Resultado = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  cursor: pointer;
+  p {
+    width: 200px;
+  }
+  img {
+    width: 100px;
+  }
+  &:hover {
+    border: 1px solid white;
+  }
+`;
+
+function Pesquisa() {
+  const [livrosPesquisados, setLivrosPesquisados] = useState([]);
+
+  return (
+    <Container>
+      <Titulo>Já sabe por onde começar?</Titulo>
+      <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
+      <Input
+        type="text"
+        placeholder="Escreva sua próxima leitura"
+        onBlur={(evento) => {
+          const textoDigitado = evento.target.value;
+          const resultadoPesquisa = livros.filter((livro) =>
+            livro.nome.includes(textoDigitado)
+          );
+          setLivrosPesquisados(resultadoPesquisa);
+        }}
+      />
+      {livrosPesquisados.map((livro) => (
+        <Resultado>
+          <p>{livro.nome}</p>
+          <img src={livro.src} alt={`Capa do livro ${livro.nome}`} />
+        </Resultado>
+      ))}
+    </Container>
+  );
+}
+
+export default Pesquisa;
