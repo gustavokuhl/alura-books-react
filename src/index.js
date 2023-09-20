b3b104c8f9a317c7025bd9b2c4e1461759f26c83
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2023-09-20 10:37:21 -0300

    fix: define header como constante

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createGlobalStyle } from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./componentes/Header";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -30,6 +31,7 @@ root.render(
   <React.StrictMode>
     <GlobalStyle />
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/favoritos" element={<p>Oi!</p>} />
         <Route path="/" element={<App />} />
